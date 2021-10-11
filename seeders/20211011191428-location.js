bba197f32b4f399617ab17851973ffa6d611710a
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-11 16:18:29 -0400

    finish location seed file

--- seeders/20211011191428-location.js
+++ seeders/20211011191428-location.js
@@ -10,85 +10,119 @@ module.exports = {
         hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
       },
       {
+        address: '23 Anderson Ave',
         city: 'Boston',
         state: 'Massachusetts',
         hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
       },
       {
+        Address: '122 Smith Dr',
         city: 'Austin',
         state: 'Texas',
         hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '12 Urna St',
+        city: 'Moscow',
+        state: 'Kentucky',
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '5 Plum St',
+        city: 'Los Angeles',
+        state: 'California',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+      },
+      {
+        address: '9 Reardon Ave',
+        city: 'Houston',
+        state: 'Texas',
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '13 Open Rd',
+        city: 'San Antonio',
+        state: 'Texas',
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '33 Shrewsbury St',
+        city: 'Jacksonville',
+        state: 'Florida',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+      },
+      {
+        address: '84 Rockwell Dr',
+        city: 'Columbus',
+        state: 'Ohio',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '34 West St',
+        city: 'Indianapolis',
+        state: 'Indiana',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '5 Ridgewood Ave',
+        city: 'Portland',
+        state: 'Oregon',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '74 Turnpike Rd',
+        city: 'Fresno',
+        state: 'California',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '22 Street Ave',
+        city: 'Atlanta',
+        state: 'Georgia',
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '146 Essex St',
+        city: 'New York',
+        state: 'New York',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '45 Lindon Ave',
+        city: 'Irvine',
+        state: 'California',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 1pm-1am'
+      },
+      {
+        address: '23 Chadbourne St',
+        city: 'Des Moines',
+        state: 'Iowa',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '12 Crestview Rd',
+        city: 'Salt Lake City',
+        state: 'Utah',
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '83 Amherst Rd',
+        city: 'Fort Lauderdale',
+        state: 'Florida',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+      },
+      {
+        address: '92 Greendale Ave',
+        city: 'Miramar',
+        state: 'Florida',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+      },
+      {
+        address: '92 Solomon Ave',
+        city: 'Seattle',
+        state: 'Washington',
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
       }
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
-      //         {
-      //   city:
-      //   state:
-      //   hours:
-      // },
     ])
   },
 
