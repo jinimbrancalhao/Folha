287b0f71ed2e37979cd792cecfa9c74954a3dc77
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-11 16:56:35 -0400

    all data seeded

--- seeders/20211011163404-menu.js
+++ seeders/20211011163404-menu.js
@@ -9,211 +9,275 @@ module.exports = {
           name: 'Lobster Bisque',
           description: 'Lobster meat cognac',
           price: 13,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Caesar Salad',
           description:
             'House made caesar dressing, grated Pecorino, and anchovies',
           price: 9,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Mushroom Risotto',
           description: 'Pan seared mushroom and spinach',
           price: 17,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Cream of Tomato Fennel',
           description: 'With aged cheddar croutons',
           price: 8,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Oyster Tempura',
           description: 'Served with choice of sweet chili or cucumber aeoli',
           price: 12,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Salmon Tartar',
           description: 'Sweet pepper relish, mango, plantain',
           price: 17,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Pork Belly',
           description: 'Smoked garlic, chimichurri, and pickled cabbage',
           price: 15,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Shrimp Cocktail',
           description:
             '7 jumbo cooked shrimp served with cocktail sauce and orange chili sauce',
           price: 11,
-          course: 'Appetizer'
+          course: 'Appetizer',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Beef Tenderloin',
           description:
             'Roasted and topped with spicy chili infusion, served medium-rare',
           price: 26,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Grilled Pork Chop',
           description:
             'Topped with butter-toasted Panko, served over a smoked pork belly',
           price: '29',
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Pan-Seared Scallops',
           description:
             'Presented with wild mushroom barley and pickled tri-color carrots',
           price: 28,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Hand-Rolled Potato Gnocchi',
           description:
             'Topped with house made marinara, roasted garlic, asparags, and cherry tomatoes',
           price: 23,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Prime Rib',
           description: 'Aged 8 oz prime rib, slow roasted in rock salt',
           price: 27,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Lobster Ravioli',
           description: 'Poached in a light bisque with wood sorrel',
           price: 26,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Seafood Risotto',
           description:
             'Jumbo shrimp, local seaa scallops, little neck clam, assorted mushrooms in a sherry cream sauce',
           price: 34,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Peppered Shrimp and Crab Cake',
           description:
             'Jumbo lump crab cakes set over mashed potatoes and baby spinach topped with jumbo shrimp',
           price: 30,
-          course: 'Entree'
+          course: 'Entree',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Bananas and berries foster',
           description: 'Bananas with fresh berries and cream',
           price: 9,
-          course: 'Dessert'
+          course: 'Dessert',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Croissant Bread Pudding',
           description: 'Flaky crust with bourbon bread pudding',
           price: 8,
-          course: 'Dessert'
+          course: 'Dessert',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Tiramisu',
           description:
             'ladyfingers dipped in a liqueur, then layered with a rich custard',
           price: 9,
-          course: 'Dessert'
+          course: 'Dessert',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Creme Brulee',
           description:
             'Traditional baked custard topped with caramelized sugar and fruit',
           price: 11,
-          course: 'Dessert'
+          course: 'Dessert',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Chocolate Mousse',
           description: 'Bitter sweet, caramel infused milk and white leche',
           price: 11,
-          course: 'Dessert'
+          course: 'Dessert',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Berry White Delight',
           description:
             'Absolut Elyx Vodka, Chambord, Godiva White Chocolate, pressed rasberry',
           price: 17,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Worthing Watermelon',
           description: 'Watermelon and cucumber infused Absolut Vodka',
           price: 16,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Dragon Mojito',
           description:
             'Bacardi Dragonberry Rum, pressed raspberrys, mint and lime',
           price: 15,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Loco Pina',
           description:
             'Avion Silver Tequila, Ancho Reyes Liquer, chipotle pinneaple puree',
           price: 14,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Spiced Old Fashioned',
           description:
             "Knob Creek Bourbon, Laird's Brandy, pear, vermouth, bitters",
           price: 8,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Whiskey Sour',
           description:
             'Jim Beam Black, Templeton Rye, Caffe Amaro, Sauternes, lemon, egg, salt',
           price: 13,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'The Gentlemen',
           description:
             "Maker's Mark Bourbon, Kahlua, Frangelico, espresso, bitters",
           price: 14,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Strawberry Daiquiri',
           description:
             'White rum, fresh lime juice, sugar syrup, fresh chopped strawberries',
           price: 8,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Blue Lagoon',
           description: 'Vodka, blue curacao liqueur, lemonade, lemon',
           price: 8,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Lemon Drop Martini',
           description:
             'Lemon juice with simple syrup, shaken with ice and served with sugared rim',
           price: 11,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           name: 'Amaretto Sour',
           description: 'Amaretto with sweet and sour served on crushed ice',
           price: 10,
-          course: 'Drinks'
+          course: 'Drinks',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
--- seeders/20211011191428-location.js
+++ seeders/20211011191428-location.js
@@ -7,121 +7,161 @@ module.exports = {
         address: '50 Water St',
         city: 'Worcester',
         state: 'Massachusetts',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '23 Anderson Ave',
         city: 'Boston',
         state: 'Massachusetts',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        Address: '122 Smith Dr',
+        address: '122 Smith Dr',
         city: 'Austin',
         state: 'Texas',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '12 Urna St',
         city: 'Moscow',
         state: 'Kentucky',
-        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '5 Plum St',
         city: 'Los Angeles',
         state: 'California',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '9 Reardon Ave',
         city: 'Houston',
         state: 'Texas',
-        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '13 Open Rd',
         city: 'San Antonio',
         state: 'Texas',
-        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '33 Shrewsbury St',
         city: 'Jacksonville',
         state: 'Florida',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '84 Rockwell Dr',
         city: 'Columbus',
         state: 'Ohio',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '34 West St',
         city: 'Indianapolis',
         state: 'Indiana',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '5 Ridgewood Ave',
         city: 'Portland',
         state: 'Oregon',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '74 Turnpike Rd',
         city: 'Fresno',
         state: 'California',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '22 Street Ave',
         city: 'Atlanta',
         state: 'Georgia',
-        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '146 Essex St',
         city: 'New York',
         state: 'New York',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '45 Lindon Ave',
         city: 'Irvine',
         state: 'California',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 1pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 1pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '23 Chadbourne St',
         city: 'Des Moines',
         state: 'Iowa',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '12 Crestview Rd',
         city: 'Salt Lake City',
         state: 'Utah',
-        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 12pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '83 Amherst Rd',
         city: 'Fort Lauderdale',
         state: 'Florida',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '92 Greendale Ave',
         city: 'Miramar',
         state: 'Florida',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 12pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         address: '92 Solomon Ave',
         city: 'Seattle',
         state: 'Washington',
-        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am'
+        hours: 'Mon-Thur: 1pm-12am. Fri-Sun: 2pm-1am',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
