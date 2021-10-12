109913e56eb02dfdd27a20e860cdfc4cb1583a6b
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-12 15:56:50 -0400

    axios installed and menu halfway

---
+++ client/src/pages/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  components: {
+
+  },
+  data: () => ({
+    menu: []
+  }),
+  mounted: function(){
+
+  },
+  methods: {
+    async GetMenu(){
+      const res = await axios.get(
+        'http://localhost:3001/api/menu'
+      )
+      this.menu = res.data
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
