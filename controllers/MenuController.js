c96fbae23533276aa167f20bc03ef9a87c060b10
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-12 11:09:08 -0400

    two menu controllers written

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,1 +1,19 @@
-const 
+const { Menu } = require('../models')
+
+const GetMenu = async (req, res) => {
+  try {
+    const menu = await Menu.findAll()
+    res.send(menu)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetAppetizers = async (req, res) => {
+  try {
+    const appetizers = await Menu.findAll({ where: { course: 'Appetizer' } })
+    res.send(appetizers)
+  } catch {
+    throw error
+  }
+}
