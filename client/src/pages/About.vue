9a6da8c2caed4374a6ba20818fbd69bd7c0337b4
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-12 22:15:58 -0400

    about get written

---
+++ client/src/pages/About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Who are we?</h2>
+    <h3>Comments</h3>
+    <div>
+      <Comment />
+    </div>
+  </div>
+</template>
+
+<script>
+import Comment from '../components/Comment.vue'
+import axios from 'axios'
+
+export default {
+  name: 'About',
+  components: {
+    Comment
+  },
+  data: () => ({
+    comments: []
+  }),
+  mounted: function() {
+    this.getComments()
+  },
+  methods: {
+    async getComments(){
+      const res = await axios.get(
+        'http://localhost:3001/api/comment'
+      )
+      this.comments = res.data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
