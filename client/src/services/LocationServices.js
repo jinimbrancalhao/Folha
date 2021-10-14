b709884f690b56ef7991d7c2e1d622887a2ce6a4
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-14 14:00:08 -0400

    get location by state service

--- client/src/services/LocationServices.js
+++ client/src/services/LocationServices.js
@@ -4,3 +4,7 @@ export const GetLocations = async () => {
   const res = await Client.get('/location')
   return res.data
 }
+
+export const GetLocationByState = async state => {
+  const res = await Client.get(`/location/${state}`)
+}
