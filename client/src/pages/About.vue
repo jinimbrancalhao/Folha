28e0cd2685304308caeafb5841b207ac9012b3c9
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-15 11:51:47 -0400

    about heading

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -2,8 +2,9 @@
   <div>
     <section>
     <h2>Who are we?</h2>
-    
+
     <p>We believe in offering more than a meal. At Folha it's always an experience. We've brought in professionals to design our restaurant, creating a comfortable and vibrant atmosphere. We've brought Chef Brancalhao who has truly brought life and passion to our menu. Our bar and lounge is run by top talent to make sure your experience is as good as possible no matter what you visit us for!</p>
+    <h3>Below is a space for the Folha community to share their experiences. We love to hear back from our valued customers!</h3>
     </section>
     <section>
       <h3>Comments</h3>
